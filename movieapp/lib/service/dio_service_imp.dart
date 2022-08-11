8192a7b65189639434625f7af898e1c7546b27a8
JacoRochaDev <jacofernandes@outlook.com>
2022-08-11 19:13:06 -0300

    criacao do dio service

---
+++ movieapp/lib/service/dio_service_imp.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:dio/src/dio.dart';
+import 'package:movieapp/service/dio_service.dart';
+
+class DioServiceImp implements DioService {
+  @override
+  Dio getDio() {
+    return Dio(
+      BaseOptions(
+        baseUrl: 'https://api.themoviedb.org/3/',
+        connectTimeout: 5000,
+        receiveTimeout: 3000,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'authorization': 'Bearer 5b0a0c5f28b81009ed6225b6a567b497'
+        },
+      ),
+    );
+  }
+}
